92b64843442e7b852a7f7a751f700887a08d5d2d
saimaboy <dhanukawickramage.edu.se.2001@gmail.com>
2025-01-10 23:08:22 +0530

    Create Server Side and Client Side project Structure

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
